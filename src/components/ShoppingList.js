7d5bcabae72dde4cd8557bfce8b717f35cf68ea1
Maryam Verdon <maryamverdon@gmail.com>
2022-03-23 21:45:02 +0100

    ajout du composant careScale

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,4 +1,5 @@
 import { plantList } from '../datas/plantList';
+import CareScale from './CareScale';
 import '../styles/ShoppingList.css';
 
 function ShoppingList() {
@@ -7,7 +8,6 @@ function ShoppingList() {
 			acc.includes(plant.category) ? acc : acc.concat(plant.category),
 		[]
 	)
-    
 
 	return (
 		<div>
@@ -19,8 +19,10 @@ function ShoppingList() {
 			<ul className='lmj-plant-list'>
 				{plantList.map((plant) => (
 					<li key={plant.id} className='lmj-plant-item'>
+						{plant.isBestSale && <span>🔥</span>}
 						{plant.name}
-						{plant.isSpecialOffer && <div className='lmj-sales'>Soldes</div>}
+						<CareScale careType='water' scaleValue={plant.water} />
+						<CareScale careType='light' scaleValue={plant.light} />
 					</li>
 				))}
 			</ul>

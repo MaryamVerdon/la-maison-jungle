e4749583a01dcfa0a63ba521533565a38d1fe277
Maryam Verdon <maryamverdon@gmail.com>
2022-03-27 16:14:55 +0200

    gestion du trie des items par catégories

--- src/components/App.js
+++ src/components/App.js
@@ -14,8 +14,8 @@ function App() {
             <Banner>
                 <img src={logo} alt='La maison jungle' className='lmj-logo' />
                 <h1 className='lmj-title'>La maison jungle</h1>
-            </Banner>
-            <div className='lmj-layout-inner'>
+            </Banner>	
+            <div className='lmj-layout-inner'>	
                 <Cart cart={cart} updateCart={updateCart} />
                 <ShoppingList cart={cart} updateCart={updateCart} />
             </div>
